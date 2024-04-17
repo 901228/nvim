8c9dabc4b929a0535fd70ffa4ebd92c87e7c3291
Emlon <901228@gmail.com>
2024-04-17 14:24:24 +0800

    add some comments to plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -89,12 +89,14 @@ return require("packer").startup(function()
         'folke/trouble.nvim',
         requires = 'nvim-tree/nvim-web-devicons',
     }
+    -- navigation bar of functions
     use {
         'SmiteshP/nvim-navic',
         requires = 'neovim/nvim-lspconfig',
     }
     use 'RRethy/vim-illuminate'
     use 'folke/flash.nvim'
+    -- to indicate buffers
     use {
         'akinsho/bufferline.nvim',
         requires = 'kyazdani42/nvim-web-devicons',
@@ -104,23 +106,26 @@ return require("packer").startup(function()
         'windwp/nvim-autopairs',
         config = function() require('nvim-autopairs').setup {} end
     }
+    -- window status line & buffer name
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    -- popup terminals
     use {
         'akinsho/toggleterm.nvim',
         tag = '*',
         -- config = function() require('toggleterm').setup {} end
     }
+    -- notification system of nvim
     use 'rcarriga/nvim-notify'
     use {
         'folke/noice.nvim',
         requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' }
     }
-
     -- color highlighter
     use 'NvChad/nvim-colorizer.lua'
+    -- toto highlight
     use {
         'folke/todo-comments.nvim',
         requires = { 'nvim-lua/plenary.nvim' }
@@ -135,5 +140,4 @@ return require("packer").startup(function()
             require("which-key").setup {}
         end
     }
-
 end)
