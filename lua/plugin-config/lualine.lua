5146de2da1a7bf5ac51978bd1269fde4e1c3310f
Emlon <901228@gmail.com>
2024-04-20 15:15:07 +0800

    add keystrokes at the status line

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -70,7 +70,19 @@ require('lualine').setup {
                 padding = 1,
             },
         },
-        lualine_c = { { 'fileformat', separator = { right = '' } } },
+        lualine_c = {
+            { 'fileformat', separator = { right = '' } },
+            {
+                require('noice').api.status.command.get,
+                cond = require('noice').api.status.command.has,
+                color = { fg = '#ff9e64' },
+            },
+            {
+                require('noice').api.status.mode.get,
+                cond = require('noice').api.status.mode.has,
+                color = { fg = '#ff9e64' },
+            },
+        },
         lualine_x = {
             {
                 'diagnostics',
