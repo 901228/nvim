231a185ff5bac61a4e915262c7b61121b20f098c
Emlon <901228@gmail.com>
2024-08-29 00:42:15 +0800

    add nvim-treesitter-textobjects

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -65,6 +65,15 @@ return {
                     scope_incremental = '<TAB>',
                 },
             },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+                    goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+                    goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+                    goto_previous_end = { [']F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+                },
+            },
         },
         config = function(_, opts)
             -- fold
@@ -75,6 +84,38 @@ return {
                 opts.ensure_installed = Util.dedup(opts.ensure_installed)
             end
             require('nvim-treesitter.configs').setup(opts)
+        end,
+    },
+
+    -- textobjects
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        event = 'VeryLazy',
+        enabled = true,
+        config = function()
+            if Util.plugin.is_loaded('nvim-treesitter') then
+                local opts = Util.plugin.opts('nvim-treesitter')
+                require('nvim-treesitter.configs').setup({ textobjects = opts.textobjects })
+            end
+
+            local move = require('nvim-treesitter.textobjects.move') ---@type table<string, fun(...)>
+            local configs = require('nvim-treesitter.configs')
+            for name, fn in ipairs(move) do
+                if name:find('goto') == 1 then
+                    move[name] = function(q, ...)
+                        if vim.wo.diff then
+                            local config = configs.get_module('textobjects.move')[name] ---@type table<string, string>
+                            for key, query in pairs(config or {}) do
+                                if q == query and key:find('[%]%[][cC]') then
+                                    vim.cmd('normal!' .. key)
+                                    return
+                                end
+                            end
+                        end
+                        return fn(q, ...)
+                    end
+                end
+            end
         end,
     },
 }
