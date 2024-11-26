979d1029d4514b484601f75b694d58b4f56761f3
901228 <901228@gmail.com>
2024-11-26 12:25:58 +0800

    add nerdy.nvim

--- lua/Mochi/plugins/editor/util.lua
+++ lua/Mochi/plugins/editor/util.lua
@@ -22,4 +22,13 @@ return {
             },
         },
     },
+
+    {
+        '2kabhishek/nerdy.nvim',
+        dependencies = {
+            'dressing.nvim',
+            'telescope.nvim',
+        },
+        cmd = 'Nerdy',
+    },
 }
