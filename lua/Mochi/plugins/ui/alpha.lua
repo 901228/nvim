90686d7b4a213279e982750bb0aac2467f617f5d
Emlon <901228@gmail.com>
2026-04-26 21:10:49 +0800

    make alpha align v_center

--- lua/Mochi/plugins/ui/alpha.lua
+++ lua/Mochi/plugins/ui/alpha.lua
@@ -9,50 +9,68 @@ return {
         },
         lazy = false,
         opts = function()
-
-            local dashboard = require("alpha.themes.dashboard")
+            local dashboard = require('alpha.themes.dashboard')
 
             dashboard.section.header.val = require('ascii').art.text.neovim.sharp
 
-            if vim.fn.has "win32" ~= 1 then
-                dashboard.section.terminal.command = "cat | lolcat --seed=24 "
-                .. os.getenv("HOME")
-                .. "/.config/nvim/static/neovim.cat"
+            if vim.fn.has('win32') ~= 1 then
+                dashboard.section.terminal.command = 'cat | lolcat --seed=24 '
+                    .. os.getenv('HOME')
+                    .. '/.config/nvim/static/neovim.cat'
             end
 
             -- Set menu
             dashboard.section.buttons.val = {
                 -- dashboard.button("l", "   Load session", "<cmd>lua require('persisted').load()<CR>"),
-                dashboard.button("e", "  > New file" , "<cmd>ene<CR>"),
-                dashboard.button("r", "  > Recent file"   , "<cmd>Telescope oldfiles<CR>"),
-                dashboard.button("f", "󰈞  > Find file", "<cmd>Telescope find_files hidden=true path_display=smart<CR>"),
-                dashboard.button("g", "  > Search file", "<cmd>Telescope live_grep path_display=smart<CR>"),
-                dashboard.button("l", "  > Lazy"   , "<cmd>Lazy<CR>"),
-                dashboard.button("s", "  > Settings" , "<cmd>e $MYVIMRC | :cd %:p:h | Neotree<CR>"),
-                dashboard.button("q", "󰈆  > Quit NVIM", "<cmd>qa!<CR>"),
+                dashboard.button('e', '  > New file', '<cmd>ene<CR>'),
+                dashboard.button('r', '  > Recent file', '<cmd>Telescope oldfiles<CR>'),
+                dashboard.button(
+                    'f',
+                    '󰈞  > Find file',
+                    '<cmd>Telescope find_files hidden=true path_display=smart<CR>'
+                ),
+                dashboard.button('g', '  > Search file', '<cmd>Telescope live_grep path_display=smart<CR>'),
+                dashboard.button('l', '  > Lazy', '<cmd>Lazy<CR>'),
+                dashboard.button('s', '  > Settings', '<cmd>e $MYVIMRC | :cd %:p:h | Neotree<CR>'),
+                dashboard.button('q', '󰈆  > Quit NVIM', '<cmd>qa!<CR>'),
             }
 
             -- Footer
             local function footer()
-                -- local total_plugins = require('packer').stats().count
+                local total_plugins = #require('lazy').plugins()
                 local v = vim.version()
-                local datetime = os.date " %Y-%m-%d   %H:%M:%S"
-                local platform = Util.os.is_win() and "" or ""
-                return string.format("%s v%d.%d.%d  %s", ' Neovim', v.major, v.minor, v.patch, datetime)
+                local datetime = os.date(' %Y-%m-%d |  %H:%M:%S')
+                local platform = Util.os.is_win() and '' or ''
+                return string.format(
+                    '%s %s | v%d.%d.%d | %s | %d plugins',
+                    platform,
+                    'Neovim',
+                    v.major,
+                    v.minor,
+                    v.patch,
+                    datetime,
+                    total_plugins
+                )
             end
 
             dashboard.section.footer.val = footer()
-            dashboard.section.footer.opts.hl = "AlphaFooter"
+            dashboard.section.footer.opts.hl = 'AlphaFooter'
 
             -- Layout
             dashboard.config.layout = {
-                { type = "padding", val = 2 },
-                dashboard.section.header,
-                -- dashboard.section.terminal,
-                { type = "padding", val = 2 },
-                dashboard.section.buttons,
-                { type = "padding", val = 1 },
-                dashboard.section.footer,
+                {
+                    type = 'group',
+                    opts = { position = 'v_center', },
+                    val = {
+                        { type = 'padding', val = 2 },
+                        dashboard.section.header,
+                        -- dashboard.section.terminal,
+                        { type = 'padding', val = 2 },
+                        dashboard.section.buttons,
+                        { type = 'padding', val = 1 },
+                        dashboard.section.footer,
+                    },
+                },
             }
             dashboard.config.opts.noautocmd = false
 
@@ -60,14 +78,12 @@ return {
         end,
         config = function(_, dashboard)
             -- close Lazy and re-open when the dashboard is ready
-            if vim.o.filetype == "lazy" then
+            if vim.o.filetype == 'lazy' then
                 vim.cmd.close()
-                vim.api.nvim_create_autocmd("User", {
+                vim.api.nvim_create_autocmd('User', {
                     once = true,
-                    pattern = "AlphaReady",
-                    callback = function()
-                        require("lazy").show()
-                    end,
+                    pattern = 'AlphaReady',
+                    callback = function() require('lazy').show() end,
                 })
             end
 
@@ -76,7 +92,7 @@ return {
                 autocmd FileType alpha setlocal nofoldenable
             ]])
 
-            require("alpha").setup(dashboard.opts)
+            require('alpha').setup(dashboard.opts)
         end,
-    }
+    },
 }
